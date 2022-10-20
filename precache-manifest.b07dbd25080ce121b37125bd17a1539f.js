2522390af58eaf555cc7b05f1b959c47ae393d65
Dorki <bartdane@yandex.ru>
2022-10-20 21:43:08 +0600

    Updates

---
+++ precache-manifest.b07dbd25080ce121b37125bd17a1539f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fd71b3f363e50b2611d",
+    "url": "/entertainment-web-app/static/css/main.0bc06cfa.chunk.css"
+  },
+  {
+    "revision": "8fd71b3f363e50b2611d",
+    "url": "/entertainment-web-app/static/js/main.8fd71b3f.chunk.js"
+  },
+  {
+    "revision": "4c4350258503dcfb093b",
+    "url": "/entertainment-web-app/static/css/1.77cbc713.chunk.css"
+  },
+  {
+    "revision": "4c4350258503dcfb093b",
+    "url": "/entertainment-web-app/static/js/1.4c435025.chunk.js"
+  },
+  {
+    "revision": "f12ae4b65877e88449fc",
+    "url": "/entertainment-web-app/static/js/runtime~main.f12ae4b6.js"
+  },
+  {
+    "revision": "ddeecd2537724562cd81321c224d0cb4",
+    "url": "/entertainment-web-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/entertainment-web-app/home/precache-manifest.a8fb67c6f4e9a2403311d1b6068704a2.js"
+  "/entertainment-web-app/precache-manifest.b07dbd25080ce121b37125bd17a1539f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/entertainment-web-app/home/index.html", {
+workbox.routing.registerNavigationRoute("/entertainment-web-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
