1307bda4525a2ee7e862bf510a32028df93f3d29
Dorki <bartdane@yandex.ru>
2022-10-20 21:37:13 +0600

    gh pages

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/home  ">
+    <BrowserRouter basename="/home">
       <App />
     </BrowserRouter>
   </React.StrictMode>
