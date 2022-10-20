31818264763cbcf8130db84d2401c7763f2804c3
Dorki <bartdane@yandex.ru>
2022-10-20 21:35:18 +0600

    Updates

---
+++ precache-manifest.cf53c7f139028c2fd57eed36458d509d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ad4f36b990afed13573",
+    "url": "/entertainment-web-app/static/css/main.0bc06cfa.chunk.css"
+  },
+  {
+    "revision": "6ad4f36b990afed13573",
+    "url": "/entertainment-web-app/static/js/main.6ad4f36b.chunk.js"
+  },
+  {
+    "revision": "4c4350258503dcfb093b",
+    "url": "/entertainment-web-app/static/css/1.77cbc713.chunk.css"
+  },
+  {
+    "revision": "4c4350258503dcfb093b",
+    "url": "/entertainment-web-app/static/js/1.4c435025.chunk.js"
+  },
+  {
+    "revision": "f12ae4b65877e88449fc",
+    "url": "/entertainment-web-app/static/js/runtime~main.f12ae4b6.js"
+  },
+  {
+    "revision": "9305fb05854181d44001772e4993cc0b",
+    "url": "/entertainment-web-app/index.html"
+  }
+];
